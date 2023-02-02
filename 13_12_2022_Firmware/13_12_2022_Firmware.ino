89149e26c5c8f5da311f8b2fbb324cf532e4104d
Vedant Kelkar <vedant.kelkar@somaiya.edu>
2023-02-02 15:31:41 +0530

    changes

---
+++ 13_12_2022_Firmware/13_12_2022_Firmware.ino
@@ -0,0 +1,120 @@
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged TO GPIO 4
+#define ONE_WIRE_BUS 4
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// Number of temperature devices found
+int numberOfDevices;
+
+// We'll use this variable to store a found device address
+DeviceAddress tempDeviceAddress; 
+
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
+ 
+#define REPORTING_PERIOD_MS     1000
+ 
+PulseOximeter pox;
+uint32_t tsLastReport = 0;
+ 
+void onBeatDetected()
+{
+    Serial.println("Beat!");
+}
+ 
+
+void setup(){
+  // start serial port
+  Serial.begin(115200);
+  
+  // Start up the library
+  sensors.begin();
+  
+  // Grab a count of devices on the wire
+  numberOfDevices = sensors.getDeviceCount();
+  
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  Serial.print(numberOfDevices, DEC);
+  Serial.println(" devices.");
+
+  // Loop through each device, print out address
+  for(int i=0;i<numberOfDevices; i++){
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i)){
+      Serial.print("Found device ");
+      Serial.print(i, DEC);
+      Serial.print(" with address: ");
+      printAddress(tempDeviceAddress);
+      Serial.println();
+    } else {
+      Serial.print("Found ghost device at ");
+      Serial.print(i, DEC);
+      Serial.print(" but could not detect address. Check power and cabling");
+    }
+  }
+   Serial.print("Initializing pulse oximeter..");
+ 
+    // Initialize the PulseOximeter instance
+    // Failures are generally due to an improper I2C wiring, missing power supply
+    // or wrong target chip
+    if (!pox.begin()) {
+        Serial.println("FAILED");
+        for(;;);
+    } else {
+        Serial.println("SUCCESS");
+    }
+     pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+ 
+    // Register a callback for the beat detection
+    pox.setOnBeatDetectedCallback(onBeatDetected);
+}
+
+void loop(){ 
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  
+  // Loop through each device, print out temperature data
+  for(int i=0;i<numberOfDevices; i++){
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i)){
+      // Output the device ID
+      Serial.print("Temperature for device: ");
+      Serial.println(i,DEC);
+      // Print the data
+      float tempC = sensors.getTempC(tempDeviceAddress);
+      Serial.print("Temp C: ");
+      Serial.print(tempC);
+      Serial.print(" Temp F: ");
+      Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
+    }
+  }
+  delay(500);
+
+      pox.update();
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+        Serial.print("Heart rate:");
+        Serial.print(pox.getHeartRate());
+        Serial.print("bpm / SpO2:");
+        Serial.print(pox.getSpO2());
+        Serial.println("%");
+ 
+        tsLastReport = millis();
+    }
+}
+
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress) {
+  for (uint8_t i = 0; i < 8; i++){
+    if (deviceAddress[i] < 16) Serial.print("0");
+      Serial.print(deviceAddress[i], HEX);
+  }
+}
--- ad8232_calib/ad8232_calib.ino
+++ ad8232_calib/ad8232_calib.ino
@@ -1,20 +1,20 @@
 void setup() {
 // initialize the serial communication:
 Serial.begin(9600);
-pinMode(10, INPUT); // Setup for leads off detection LO +
-pinMode(11, INPUT); // Setup for leads off detection LO -
- 
+pinMode(25, INPUT); // Setup for leads off detection LO +
+pinMode(26, INPUT); // Setup for leads off detection LO -
+pinMode(A0,INPUT);
 }
  
 void loop() {
  
-if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
-Serial.println('!');
-}
-else{
+//if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
+//Serial.println('!');
+//}
+//else{
 // send the value of analog input 0:
 Serial.println(analogRead(A0));
-}
+//}
 //Wait for a bit to keep serial data from saturating
-delay(1);
+delay(10);
 }
