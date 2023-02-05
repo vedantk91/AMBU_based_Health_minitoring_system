4688671e32ccf13b8e22d0b236ed49378348fd23
Vedant Kelkar <vedant.kelkar@somaiya.edu>
2023-02-05 22:47:08 +0530

    data logging

--- ad8232_calib/ad8232_calib.ino
+++ ad8232_calib/ad8232_calib.ino
@@ -8,13 +8,13 @@ pinMode(A0,INPUT);
  
 void loop() {
  
-//if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
-//Serial.println('!');
-//}
-//else{
-// send the value of analog input 0:
+if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
+Serial.println('!');
+}
+else{
+ send the value of analog input 0:
 Serial.println(analogRead(A0));
-//}
+}
 //Wait for a bit to keep serial data from saturating
 delay(10);
 }
