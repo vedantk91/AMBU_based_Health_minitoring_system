18f2eff055bcf271c40e970889c14b26be84cb31
isha-makwana <imak1811@gmail.com>
2023-03-16 23:47:42 +0530

    Create combine_code.ino

---
+++ combine_code/combine_code.ino
@@ -0,0 +1,156 @@
+
+//max30100 part starts
+#include <Wire.h>                                                //max30100 part 
+#include "MAX30100_PulseOximeter.h"
+ 
+#define REPORTING_PERIOD_MS     1000
+ 
+PulseOximeter pox;
+uint32_t tsLastReport = 0;
+ 
+
+ //ds18b20 part 
+#include <OneWire.h>                                                          //ds18b20 part
+#include <DallasTemperature.h>
+
+// Data wire is plugged TO GPIO 4
+#define ONE_WIRE_BUS 32
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// Number of temperature devices found
+int numberOfDevices;
+
+// We'll use this variable to store a found device address
+DeviceAddress tempDeviceAddress; 
+
+
+
+void onBeatDetected()
+{
+    Serial.println("Beat!");
+}
+
+
+void setup()
+{
+    Serial.begin(115200);
+    
+    //ds18b20 setup part 
+    // Start up the library
+
+  sensors.begin();
+  
+  // Grab a count of devices on the wire
+  numberOfDevices = sensors.getDeviceCount();
+  
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  Serial.print(numberOfDevices, DEC);
+  Serial.println(" devices.");
+
+  // Loop through each device, print out address
+  for(int i=0;i<numberOfDevices; i++){
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i)){
+      Serial.print("Found device ");
+      Serial.print(i, DEC);
+      Serial.print(" with address: ");
+      printAddress(tempDeviceAddress);
+      Serial.println();
+    } else {
+      Serial.print("Found ghost device at ");
+      Serial.print(i, DEC);
+      Serial.print(" but could not detect address. Check power and cabling");
+    }
+  }
+
+  // //ad8232
+  // pinMode(25, INPUT); // Setup for leads off detection LO +
+  // pinMode(26, INPUT); // Setup for leads off detection LO -
+  // pinMode(A0,INPUT);
+
+
+    //max30100 setup part starts
+    Serial.print("Initializing pulse oximeter..");                                         //max30100 setup part
+ 
+    // Initialize the PulseOximeter instance
+    // Failures are generally due to an improper I2C wiring, missing power supply
+    // or wrong target chip
+    if (!pox.begin()) {
+        Serial.println("FAILED");
+        for(;;);
+    } else {
+        Serial.println("SUCCESS");
+    }
+     pox.setIRLedCurrent(MAX30100_LED_CURR_7_6MA);
+ 
+    // Register a callback for the beat detection
+    pox.setOnBeatDetectedCallback(onBeatDetected);
+}
+ 
+void loop()
+{
+   
+    // ds18b20 part loop 
+      sensors.requestTemperatures(); // Send the command to get temperatures                       //ds18b20 part 
+  
+  // Loop through each device, print out temperature data
+  for(int i=0;i<numberOfDevices; i++){
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i)){
+      // Output the device ID
+      Serial.print("Temperature for device: ");
+      Serial.println(i,DEC);
+      // Print the data
+      float tempC = sensors.getTempC(tempDeviceAddress);
+      Serial.print("Temp C: ");
+      Serial.print(tempC);
+      Serial.print(" Temp F: ");
+      Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
+    }
+  }
+  delay(500);
+
+
+//   //ad8232
+//   if((digitalRead(10) == 1)||(digitalRead(11) == 1)){
+//      Serial.println('!');
+//   }
+//   else{
+//     //send the value of analog input 0:
+//     Serial.println(analogRead(A0));
+//   }
+// //Wait for a bit to keep serial data from saturating
+//   delay(10);
+
+    
+    // max30100 part loop
+    // Make sure to call update as fast as possible
+    pox.update();                                                                //max30100 part
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+        Serial.print("Heart rate:");
+        Serial.print(pox.getHeartRate());
+        Serial.print("bpm / SpO2:");
+        Serial.print(pox.getSpO2());
+        Serial.println("%");
+ 
+        tsLastReport = millis();
+    }
+}
+
+
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress) {
+  for (uint8_t i = 0; i < 8; i++){
+    if (deviceAddress[i] < 16) Serial.print("0");
+      Serial.print(deviceAddress[i], HEX);
+  }
+}
+
+
